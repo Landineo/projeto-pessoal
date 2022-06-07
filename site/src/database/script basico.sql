4be7dfb89fdbfb2e013a768083108a218dc38791
Lucas Landim <lucas.rodrigues@sptech.school>
2022-06-07 00:58:25 -0300

    instalada API, faltando configurar

--- site/src/database/script basico.sql
+++ site/src/database/script basico.sql
@@ -6,6 +6,7 @@ create table usuario (
 id int primary key auto_increment,
 login varchar(45),
 email varchar(45),
+senha varchar(45),
 nome varchar(45),
 tipo_fav varchar(40),
 poke_fav varchar(45));
@@ -26,3 +27,11 @@ fkUsuario int,
 foreign key (fkUsuario) references usuario(id),
 comentario varchar(280));
 
+select * from usuario;
+
+insert into usuario(login, email, nome) values 
+('lando', 'lando@lando.com', '123'),
+('lanbo', 'lanbo@lanbo.com', '123'),
+('lanjo', 'lanjo@lanjo.com', '123');
+
+select * from usuario where email = null OR login = 'lando' && nome = '123';
